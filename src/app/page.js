5bce6a72949b32c39b47c3b89e9cc213e56411ed
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-11 14:27:08 +0100

    app

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Home () {
-  return <div> </div>
+export default async function Home () {
+  let response = await fetch('https://dummyjson.com/products')
+  let data = await response.json()
+
+  return (
+    <div>
+      {data.products.map(product => (
+        <div key={product.id}>
+          <Image
+            src={product.thumbnail}
+            width={250}
+            height={250}
+            alt={product.title}
+          />
+          <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
+        </div>
+      ))}
+    </div>
+  )
 }
