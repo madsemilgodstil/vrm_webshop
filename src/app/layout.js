08444f8ccebfb974b379862f4bb6b513ac2426fd
Rasmus Olsen <rasmusolsen@Rasmuss-MacBook-Pro.local>
2024-11-11 15:57:53 +0100

    related start

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-import Navigation from "@/components/navigation/Navigation";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -22,12 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header>
-          <Navigation />
-        </header>
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
