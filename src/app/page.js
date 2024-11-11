f7072f0df4e316724dfa5d8875989398d828345c
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-11 14:42:27 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default async function Home () {
             height={250}
             alt={product.title}
           />
-          <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
+          <Link href={`/singleproduct/${product.id}`}>{product.title}</Link>
         </div>
       ))}
     </div>
