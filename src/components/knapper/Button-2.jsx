4a2659f8010947dce716da8e77355fd1694972e3
vabr0002 <vabr0002@stud.kea.dk>
2024-11-12 12:59:15 +0100

    products

--- src/components/knapper/Button-2.jsx
+++ src/components/knapper/Button-2.jsx
@@ -2,7 +2,7 @@ const Button2 = ({ link, text }) => {
   return (
     <a
       href={link}
-      className="hover:bg-white hover:text-[#B6CCDA] px-6 py-1 rounded-lg transition duration-300 ease-in-out cursor-pointer"
+      className=" text-white bg-black  hover:bg-white hover:text-[#000000] px-6 py-1 rounded-lg transition duration-300 ease-in-out cursor-pointer"
     >
       {text}
     </a>
