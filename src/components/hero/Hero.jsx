82bcbdc56f507281736fe0f4c916cdfbe7d42890
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-12 14:28:10 +0100

    hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,11 @@
 const Hero = () => {
   return (
-    <div className="relative ">
-      {/* Baggrundsbillede */}
+    <div className="relative w-screen h-screen overflow-hidden">
       <img
         src="/images/miguel-sousa-hUBeoOI0TmA-unsplash.jpg"
         alt="Hero Image"
-        className="h-screen  object-cover"
+        className="w-full h-full object-cover"
       />
-
-      {/* Overlay med tekst og knap */}
       <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
         <h1 className="text-3xl font-bold mb-4">Velkommen til vores side!</h1>
         <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
