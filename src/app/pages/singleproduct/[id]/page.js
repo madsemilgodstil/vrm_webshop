99cd5add6811df953290bfe47c7cbc451f3eef1d
vabr0002 <vabr0002@stud.kea.dk>
2024-11-13 13:36:58 +0100

    style på single og related

--- src/app/pages/singleproduct/[id]/page.js
+++ src/app/pages/singleproduct/[id]/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import RelatedProducts from "@/components/related/Related";
+import ButtonSec from "@/components/knapper/ButtonSec";
 
 export default async function SingleProductPage({ params }) {
   const { id } = params;
@@ -12,16 +13,39 @@ export default async function SingleProductPage({ params }) {
     const product = await response.json();
 
     return (
-      <div className="main">
-        <div>
-          <h1>{product.title}</h1>
-          <Image src={product.thumbnail} width={250} height={250} alt={product.title} />
-          <p>{product.description}</p>
-          <p>Price: ${product.price}</p>
-        </div>
-        <div>
-          {/* Pass the category and current product ID */}
-          <RelatedProducts category={product.category} currentProductId={product.id} />
+      <div className="main container mx-auto my-8 px-4">
+        <div className="bg-white p-8">
+          <div className="flex flex-col md:flex-row items-start gap-16">
+            <div>
+              <Image
+                src={product.thumbnail}
+                width={500}
+                height={500}
+                alt={product.title}
+                className="rounded-md"
+              />
+            </div>
+            <div className="flex-1 flex flex-col">
+              <h1 className="text-3xl font-bold mb-6">{product.title}</h1>
+              <p className="text-gray-700 text-lg leading-relaxed mb-4">
+                {product.description}
+              </p>
+
+              <div className="mt-72 flex justify-between items-end">
+                <p className="text-xl font-semibold">Price: ${product.price}</p>
+                <ButtonSec text="Køb" />
+              </div>
+            </div>
+          </div>
+
+          <div className="border-t border-gray-300 my-8"></div>
+
+          <div>
+            <RelatedProducts
+              category={product.category}
+              currentProductId={product.id}
+            />
+          </div>
         </div>
       </div>
     );
