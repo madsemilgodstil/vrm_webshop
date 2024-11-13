409b702b9cc7304a70508a9e7f67e8c30bb15a1e
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-13 13:36:01 +0100

    merge

--- src/components/basket/Basket.jsx
+++ src/components/basket/Basket.jsx
@@ -1,10 +1,21 @@
-import Payment from "@/components/payment/Payment";
 import Button from "@/components/knapper/ButtonPri";
 
 const Basket = () => {
   return (
     <div className="sticky top-5 right-0 border border-gray-200 rounded-lg shadow-lg p-4">
-      <Payment />
+      <div className="space-y-4 mx-auto">
+        <div className="product flex items-center">
+          <p className="w-16 h-16 bg-gray-200 mr-4 flex items-center justify-center">
+            Images her
+          </p>
+          <div className="content">
+            <h2>Produktnavn</h2>
+            <p>Antal:</p>
+            <p>Pri:</p>
+          </div>
+        </div>
+        <p className="text-lg">I alt at betale: ,-</p>
+      </div>
       <Button text="Gå til kurv" />
     </div>
   );
