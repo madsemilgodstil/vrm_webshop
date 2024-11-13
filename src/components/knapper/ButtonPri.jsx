fe50b73811641b8104ba6b56979e36a1ab0b9ae5
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-13 12:27:35 +0100

    button fix

--- src/components/knapper/ButtonPri.jsx
+++ src/components/knapper/ButtonPri.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const ButtonPri = ({ link, text }) => {
+const ButtonPri = ({ link = "/", text }) => {
   return (
     <Link href={link}>
       <span className="bg-slate-50 text-black hover:bg-black hover:text-white px-6 py-3 rounded-full transition duration-300 ease-in-out cursor-pointer">
--- src/components/knapper/ButtonSec.jsx
+++ src/components/knapper/ButtonSec.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
-const ButtonSec = ({ link, text }) => {
+const ButtonSec = ({ link = "/", text }) => {
   return (
-    <Link href={link}>
-      <a className="text-white bg-black border hover:bg-white hover:text-[#000000] border-2 border-black hover:border-black px-6 py-1 rounded-full transition duration-300 ease-in-out cursor-pointer">
-        {text}
-      </a>
+    <Link
+      href={link}
+      className="text-white bg-black border hover:bg-white hover:text-[#000000] border-2 border-black hover:border-black px-6 py-1 rounded-full transition duration-300 ease-in-out cursor-pointer"
+    >
+      {text}
     </Link>
   );
 };
