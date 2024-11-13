cc2aaa531910e1dafb44bbb5c6f96fcac32b69f7
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-13 14:18:26 +0100

    fix4

--- src/app/pages/payment/page.js
+++ src/app/pages/payment/page.js
@@ -4,7 +4,21 @@ import ButtonPri from '@/components/knapper/ButtonPri'
 export default function PaymentPage () {
   return (
     <div className='max-w-[700px] mx-auto my-10'>
-      <Basket />
+      <div className='space-y-4 mx-auto'>
+        <div className='product flex items-center'>
+          <p className='w-16 h-16 bg-gray-200 mr-4 flex items-center justify-center'>
+            Images her
+          </p>
+          <div className='content'>
+            <h2>Produktnavn</h2>
+            <p>Antal:</p>
+            <p>Pri:</p>
+          </div>
+        </div>
+        <p className='text-lg'>I alt at betale: ,-</p>
+        <br />
+        <ButtonPri text='Betal her' link='/pages/payment/' />
+      </div>
     </div>
   )
 }
