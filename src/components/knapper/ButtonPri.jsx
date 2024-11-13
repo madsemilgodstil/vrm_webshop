8aaa80d29b151202ab10e687fc8837e990e84e28
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-13 12:23:53 +0100

    button update

--- src/components/knapper/ButtonPri.jsx
+++ src/components/knapper/ButtonPri.jsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
+
 const ButtonPri = ({ link, text }) => {
   return (
-    <a
-      href={link}
-      className=" bg-slate-50 text-black hover:bg-black hover:text-white px-6 py-3 rounded-full transition duration-300 ease-in-out cursor-pointer"
-    >
-      {text}
-    </a>
+    <Link href={link}>
+      <span className="bg-slate-50 text-black hover:bg-black hover:text-white px-6 py-3 rounded-full transition duration-300 ease-in-out cursor-pointer">
+        {text}
+      </span>
+    </Link>
   );
 };
 
