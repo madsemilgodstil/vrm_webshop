c43d2584d554d5921ab1047c36f589da7ee73b2a
vabr0002 <vabr0002@stud.kea.dk>
2024-11-14 10:14:35 +0100

    lidt ændringer

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,31 @@
+"use client";
+
 import ButtonPri from "../knapper/ButtonPri";
+import Image from "next/image";
+
+import hero_billede from "../../../public/images/reuben-mansell-nwOip8AOZz0-unsplash.jpg";
 
 const Hero = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <img
-        src="/images/miguel-sousa-hUBeoOI0TmA-unsplash.jpg"
-        alt="Hero Image"
-        className="w-full h-full object-cover"
+      <Image
+        src={hero_billede}
+        alt="hero-billede"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="top"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
-        <h1 className="text-3xl font-bold mb-4">Velkommen til vores side!</h1>
-        <ButtonPri text="Køb Nu" link="/pages/products"></ButtonPri>
+
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-15 text-white gap-8">
+        <h1 className="text-3xl font-bold">Velkommen til vores side</h1>
+        <p className="mx-12 font-bold max-w-3xl text-center">
+          Velkommen til vores købsportal – din ultimative destination for alt
+          inden for skønhed og stil! Vi tilbyder et håndplukket udvalg af
+          beauty-produkter, elegante tasker, smukke kjoler og luksuriøs
+          skincare. Uanset om du ønsker at opgradere din garderobe, forkæle din
+          hud eller finde det perfekte tilbehør, har vi noget for enhver smag.
+        </p>
+        <ButtonPri text="Gå på opdagelse" link="/pages/products"></ButtonPri>
       </div>
     </div>
   );
